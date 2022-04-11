a2f458087520bbded459581d63dd8fca98f86b73
Theodlz <theophile.dulaz@gmail.com>
2022-04-11 15:52:12 +0200

    test

--- tests/test_skyportal_api.py
+++ tests/test_skyportal_api.py
@@ -1,6 +1,7 @@
 import skyportal_api as skyportal_api
 import yaml
 import os
+
 with open(
         os.path.abspath(os.path.join(os.path.dirname(__file__))) + "/../skyportal/.tokens.yaml", "r"
     ) as stream:
@@ -11,8 +12,8 @@ with open(
 
 skyportal_token = conf_skyportal["INITIAL_ADMIN"]
 
-def test_get_all_groups_id():
-    status, data = skyportal_api.get_all_group_ids('localhost:5000', skyportal_token)
+def test_get_all_groups_id(super_admin_token):
+    status, data = skyportal_api.get_all_group_ids('localhost:5000', super_admin_token)
     assert status == 200
     assert data is not None
 
