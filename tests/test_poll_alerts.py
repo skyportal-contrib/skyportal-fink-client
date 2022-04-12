6258b7e34b2753fe605f3406a6feb4cc8bf9ac72
Theodlz <theophile.dulaz@gmail.com>
2022-04-12 15:28:34 +0200

    unit tests

--- tests/test_poll_alerts.py
+++ tests/test_poll_alerts.py
@@ -94,7 +94,7 @@ def test_poll_alerts():
     if conf["testing"] == True:
         print("Using fake alerts for testing")
         schema = os.path.abspath(
-            os.path.join(os.path.dirname(__file__), "/schemas/schema_test.avsc")
+            os.path.join(os.path.dirname(__file__), "schemas/schema_test.avsc")
         )
         print(f"Schema used: {schema}")
         consumer = AlertConsumer(topics, myconfig, schema_path=schema)
